dc16e333d22d544107c5e6b864f0a57cd390a701
Cassandra Beaulaurier <cassbeau@uw.edu>
2014-11-17 12:16:24 -0800

    End of class.

--- js/app.js
+++ js/app.js
@@ -6,3 +6,51 @@
 // lat: 47.655
 // lng: -122.3080
 
+$(document).ready(function() {
+    function createMap(center, zoom) {
+        var mapElem = document.getElementById('map');
+        var map = new google.maps.Map(mapElem, {
+            center: center,
+            zoom: zoom
+        });
+
+        var marker = new google.maps.Marker({
+            position: center,
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
+
+        var infoWindow = new google.maps.InfoWindow();
+        infoWindow.setContent('<p>Here I am!</p>');
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.open(map, marker);
+            map.panTo(marker.getPosition());
+        });
+    } // createMap()
+
+    function onGeoSuccess(position) {
+        var center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        createMap(center, 14);
+    } // onGeoSuccess()
+
+    function onGeoError(err) {
+        console.log(error);
+    } // onGeoError()
+
+    var uwCoords = {
+        lat: 47.655,
+        lng: -122.3080
+    };
+
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, {
+            enableHighAccuracy: true    // enables GPS
+        });
+    } else {
+        createMap(uwCoords, 10);
+    }
+}); // ready function
